95d3ec1ce35624ba9bafb3f69121c0f0c6df2f65
Darya Tsukrovskaya <darya.raikhert.31@mail.ru>
2023-06-01 00:22:48 +0300

    some changes

---
+++ servlet_hw/src/main/java/by/teachmeskills/servletExample/Calculator.java
@@ -0,0 +1,24 @@
+package by.teachmeskills.servletExample;
+
+public class Calculator {
+    public static int sum(int num1, int num2) {
+        int result;
+        return result = num1 + num2;
+    }
+
+    public static int sub(int num1, int num2) {
+        int result;
+        return result = num1 - num2;
+    }
+
+    public static int mult(int num1, int num2) {
+        int result;
+        return result = num1 * num2;
+    }
+
+    public static int div(int num1, int num2) {
+        int result;
+        return result = num1 / num2;
+    }
+
+}
---
+++ servlet_hw/src/main/java/by/teachmeskills/servletExample/MainServlet.java
@@ -0,0 +1,30 @@
+package by.teachmeskills.servletExample;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+    @WebServlet("/math")
+    public class MainServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.setContentType("text/html");
+            int num1 = 8;
+            int num2 = 4;
+            String sumResult = num1 + " + " + num2 + " = " + Calculator.sum(num1, num2);
+            String subResult = num1 + " - " + num2 + " = " + Calculator.sub(num1, num2);
+            String multResult = num1 + " * " + num2 + " = " + Calculator.mult(num1, num2);
+            String divisResult = num1 + " / " + num2 + " = " + Calculator.div(num1, num2);
+            req.setAttribute("sum", sumResult);
+            req.setAttribute("sub", subResult);
+            req.setAttribute("mult", multResult);
+            req.setAttribute("divis", divisResult);
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/index.jsp");
+            requestDispatcher.forward(req, resp);
+        }
+    }
